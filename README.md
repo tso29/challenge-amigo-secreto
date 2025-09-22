658b75fbb75038f38fa1d9a5c9ebe44d8fbd4c76
Tayra <sotomezatayra@gmail.com>
2025-09-22 15:14:13 -0500

    Se han hecho las funcionalidades para el juego y se ha creado archivo README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Challenge Amigo Secreto
+
+Juego de "Amigo Secreto" hecho con **HTML, CSS y Javascript**.
+
+## Funcionalidades
+* Agregar nombres de amigos a una lista.
+* Mostrar los nombres ingresados.
+* Sortear un amigo secreto al azar. 
+
+- `Desarrollado por`: Tayra Soto-
--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    mostrarAmigos();
+
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+    
+}
+
+function mostrarAmigos() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear. Agrega nombres.");
+        return;
+    }
+
+    const indiceAleat = Math.floor(Math.random() * amigos.length);
+    const amigoSecreto = amigos[indiceAleat];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    const li = document.createElement("li");
+    li.textContent = `El amigo secreto es: ${amigoSecreto}`;
+    resultado.appendChild(li);
+}
